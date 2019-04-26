4ea3e55f39df62f14885019c4e6a8ed260617562
shishaodong <shishaodong@cmhi.chinamobile.com>
2019-04-26 21:13:38 +0800

    2.0.1

--- src/main/java/com/github/springbootredis/config/ConfigRedisTemplate.java
+++ src/main/java/com/github/springbootredis/config/ConfigRedisTemplate.java
@@ -1,8 +1,17 @@
 package com.github.springbootredis.config;
 
-import org.springframework.beans.factory.annotation.Autowired;
+
+import com.alibaba.fastjson.support.spring.FastJsonRedisSerializer;
+import com.github.springbootredis.pojo.Person;
+import org.jetbrains.annotations.Contract;
+import org.jetbrains.annotations.NotNull;
+import org.springframework.context.annotation.Bean;
 import org.springframework.data.redis.connection.RedisConnectionFactory;
+import org.springframework.data.redis.core.RedisTemplate;
+import org.springframework.data.redis.serializer.StringRedisSerializer;
 import org.springframework.stereotype.Component;
+
+import javax.annotation.Resource;
 
 /**
  * <p>
@@ -17,49 +26,49 @@ import org.springframework.stereotype.Component;
 @Component
 public class ConfigRedisTemplate {
 
-    @Autowired
+    @Resource
     private RedisConnectionFactory factory;
 
-//    @Resource(name = "Factory2")
-//    private RedisConnectionFactory connectionFactory2;
-//
-//    @Resource(name = "Factory6")
-//    private RedisConnectionFactory connectionFactory6;
-//
-//    @Resource(name = "Factory8")
-//    private RedisConnectionFactory connectionFactory8;
-//
-//    @Bean("RedisTemplateFastJson2")
-//    public RedisTemplate<String, String> getRedisTemplate2() {
-//        RedisTemplate<String, String> template = new RedisTemplate<>();
-//        template.setConnectionFactory(connectionFactory2);
-//        template.setKeySerializer(new StringRedisSerializer());
-//        template.setValueSerializer(new StringRedisSerializer());
-//        template.afterPropertiesSet();
-//        return template;
-//    }
-//
-//
-//    @Bean("RedisTemplateFastJson6")
-//    public RedisTemplate<String, Person> getRedisTemplate6() {
-//        return getRedisTemplate(connectionFactory6);
-//    }
-//
-//    @Bean("RedisTemplateFastJson8")
-//    public RedisTemplate<String, Person> getRedisTemplate8() {
-//        return getRedisTemplate(connectionFactory8);
-//    }
-//
-//    @NotNull
-//    @Contract(pure = true)
-//    private RedisTemplate<String, Person> getRedisTemplate(RedisConnectionFactory connectionFactory) {
-//        RedisTemplate<String, Person> template = new RedisTemplate<>();
-//        template.setConnectionFactory(connectionFactory);
-//        template.setKeySerializer(new StringRedisSerializer());
-//        template.setValueSerializer(new FastJsonRedisSerializer<>(Person.class));
-//        template.afterPropertiesSet();
-//        return template;
-//    }
-//
+    @Resource(name = "Factory1")
+    private RedisConnectionFactory connectionFactory1;
+
+    @Resource(name = "Factory2")
+    private RedisConnectionFactory connectionFactory2;
+
+    @Resource(name = "Factory3")
+    private RedisConnectionFactory connectionFactory3;
+
+    @Bean("RedisTemplateFastJson1")
+    public RedisTemplate<String, String> getRedisTemplate2() {
+        RedisTemplate<String, String> template = new RedisTemplate<>();
+        template.setConnectionFactory(connectionFactory1);
+        template.setKeySerializer(new StringRedisSerializer());
+        template.setValueSerializer(new StringRedisSerializer());
+        template.afterPropertiesSet();
+        return template;
+    }
+
+
+    @Bean("RedisTemplateFastJson2")
+    public RedisTemplate<String, Person> getRedisTemplate6() {
+        return getRedisTemplate(connectionFactory2);
+    }
+
+    @Bean("RedisTemplateFastJson3")
+    public RedisTemplate<String, Person> getRedisTemplate8() {
+        return getRedisTemplate(connectionFactory3);
+    }
+
+    @NotNull
+    @Contract(pure = true)
+    private RedisTemplate<String, Person> getRedisTemplate(RedisConnectionFactory connectionFactory) {
+        RedisTemplate<String, Person> template = new RedisTemplate<>();
+        template.setConnectionFactory(connectionFactory);
+        template.setKeySerializer(new StringRedisSerializer());
+        template.setValueSerializer(new FastJsonRedisSerializer<>(Person.class));
+        template.afterPropertiesSet();
+        return template;
+    }
+
 
 }
