7e2514899315754d1b74380ab3e1910aa82a6ae1
shishaodong <shishaodong@cmhi.chinamobile.com>
2019-05-09 11:17:52 +0800

    Controller

---
+++ src/main/java/com/github/springbootredis/controller/PersonController.java
@@ -0,0 +1,17 @@
+package com.github.springbootredis.controller;
+
+import org.springframework.web.bind.annotation.RestController;
+
+/**
+ * <p>
+ * 创建时间为 12:40 2019-04-27
+ * 项目名称 spring-boot-redis
+ * </p>
+ *
+ * @author 石少东
+ * @version 0.0.1
+ * @since 0.0.1
+ */
+@RestController
+public class PersonController {
+}
